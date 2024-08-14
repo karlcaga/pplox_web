fc4349881cbf6e338dff0adde41366f4c4e37b43
Karl Cagalawan <karl.cagalawan@gmail.com>
2024-08-14 17:01:32 -0400

    Change database name and user to pl0x

--- pplox_web/settings.py
+++ pplox_web/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'pplox_web.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='postgresql://postgres:postgres@localhost:5432/pl0x',
         conn_max_age=600    
     )
 }
