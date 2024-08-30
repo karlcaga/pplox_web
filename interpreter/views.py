8fa4ff55842d2b534aec1104513c6bbf49f6f6ae
Karl Cagalawan <karl.cagalawan@gmail.com>
2024-08-29 23:22:05 -0400

    Update pplox to v0.0.5, adding interpreter output

--- interpreter/views.py
+++ interpreter/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from pplox.scanner import Scanner
-from pplox.parser import Parser
+from pplox.parser import Parser, ParseError
 from pplox.ast_printer import AstPrinter
+from pplox.interpreter import Interpreter, to_string
 
 # Create your views here.
 def index(request):
@@ -21,7 +22,13 @@ def runcode(request):
             parser = Parser(tokens)
             expr = parser.parse()
             output += AstPrinter().print(expr)
-        except:
+            try:
+                interpreter = Interpreter()
+                output += to_string(interpreter.evaluate(expr))
+            except:
+                output += "Failed to evaluate"
+        except ParseError:
             output += "Failed to parse"
         context = {"output" : output}
         return render(request, 'index.html', context=context)
+    
